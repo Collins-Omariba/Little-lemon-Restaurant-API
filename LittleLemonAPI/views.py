2610e4883762950927be8c85f56ac02a2f9a07b7
JimmyTron <oduorjames@hotmail.com>
2023-01-20 13:19:27 +0300

    Removed repeated imports
    Removed the repeated instance and consolidated into one line
    
    	modified:   LittleLemonAPI/views.py

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,6 +1,5 @@
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser,IsAuthenticated
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import Group, User
 from rest_framework import viewsets, generics, status
